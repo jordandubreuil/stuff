192a59390e9fbe35106fe639419fc09ab71cfef3
Jordan Dubreuil <jdubreuil@neit.edu>
2019-01-26 23:20:05 -0500

    Commit

---
+++ Assets/Scripts/FollowARCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowARCamera : MonoBehaviour
+{
+
+    public GameObject cameraOBJ;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        cameraOBJ = GameObject.FindGameObjectWithTag("MainCamera");
+        offset = transform.position - cameraOBJ.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = cameraOBJ.transform.position + offset;
+    }
+}
